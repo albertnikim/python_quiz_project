1e681bbdc311d7915e76fcdab99626d611fb11fd
Albert Neczajewski <100307127+albertnikim@users.noreply.github.com>
2022-06-12 13:35:41 +0200

    Update questions_gothic.py

--- questions_gothic.py
+++ questions_gothic.py
@@ -1,10 +1,12 @@
 question_bank_gothic = {
     'Which camp is led by Gomez? A.Old Camp B.New Camp C.Sect Camp D.None of the above': 'A',
     'What is the name of the main character? A.Marvin B.Diego C.Bilbo D.Nobody knows': 'D',
+    'What is the name of the leader of Mercenaries? A.Gomez B.Lee C.Angar D.Thomas': 'B',
     'Which of these is a friend of the main character? A.Diego B.Gorn C.Lestern D.Milten': 'ABCD',
     'Which of these weapons are available in the game? A.Bows B.Guns C.Swords D.Lances': 'AC',
     'Is Rhobar II a king of Myrtana? Type True or False' : 'True',
     'Is Xardas a water mage? Type True or False': 'False',
     'The best sword in the game is called U_____l Enter the missing letters' : 'rizie',
     'The final boss in the game is called S_____r Enter the missing letters': 'leepe',
+    'The only necromancer in the game is called X____s Enter the missing letters': 'arda',
 }
