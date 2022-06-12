e3081f9d0d7730de64d280ae24ae100211b1f68d
Albert Neczajewski <100307127+albertnikim@users.noreply.github.com>
2022-06-12 13:35:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,85 @@
-from questions_gothic import question_bank_gothic
-questions = list(question_bank_gothic.keys())
-answers = list(question_bank_gothic.values())
+from question_bank_gothic import question_bank_gothic
+questionsG = list(question_bank_gothic.keys())
+answersG = list(question_bank_gothic.values())
+
+from question_bank_witcher import question_bank_witcher
+questionsW = list(question_bank_witcher.keys())
+answersW = list(question_bank_witcher.values())
+
+import random
+question_numG = len((question_bank_gothic.keys()))
+question_numW = len((question_bank_witcher.keys()))
+
+import csv
+import datetime
 
 def gothic_quiz():
     print("Welcome to the Gothic quiz!")
-    nickname()
+
+    nick = str(input("Enter your nickname:"))
+    print(f'Hello {nick}!')
+
     score = 0
-    for i in range(8):
-        print([i+1], questions[i])
+
+    randi = random.sample(range(0, question_numG), question_numG)
+    for i in range(question_numG):
+
+        print([i+1], questionsG[randi[i]])
         ans = str(input())
-        if answers[i] == ans:
+        if answersG[randi[i]] == ans:
             score += 1
             print(f'Correct! Your score is now: {score}')
-        else:
+        elif ans=='':
+            print(f'No answer! Your score is now: {score}')
+        elif answersG[randi[i]] != ans:
             score -= 1
             print(f'Wrong! Your score is now: {score}')
-    if score < 4:
+
+    max_half = question_numG/2
+    if score < max_half:
         print(f'Your final score is {score}. Try again!')
     else:
         print(f'Your final score is {score}. Well done!')
 
-def witcher_quiz():
-    print("Witcher")
-    nickname()
+        outputFile = open('resultsG.csv', 'a', newline='')
+        outputWriter = csv.writer(outputFile)
+        currentDateTime = datetime.datetime.now()
+        outputWriter.writerow([currentDateTime, nick, score])
+        outputFile.close()
 
-def nickname():
+def witcher_quiz():
+    print("Welcome to the Witcher quiz!")
+
     nick = str(input("Enter your nickname:"))
     print(f'Hello {nick}!')
+
+    score = 0
+    randi = random.sample(range(0, question_numW), question_numW)
+    for i in range(question_numW):
+
+        print([i + 1], questionsW[randi[i]])
+        ans = str(input())
+        if answersW[randi[i]] == ans:
+            score += 1
+            print(f'Correct! Your score is now: {score}')
+        elif ans == '':
+            print(f'No answer! Your score is now: {score}')
+        elif answersW[randi[i]] != ans:
+            score -= 1
+            print(f'Wrong! Your score is now: {score}')
+
+    max_half = question_numW / 2
+    if score < max_half:
+        print(f'Your final score is {score}. Try again!')
+
+    else:
+        print(f'Your final score is {score}. Well done!')
+
+        outputFile = open('resultsW.csv', 'a', newline='')
+        outputWriter = csv.writer(outputFile)
+        currentDateTime = datetime.datetime.now()
+        outputWriter.writerow([currentDateTime, nick, score])
+        outputFile.close()
 
 def help():
     print("All helpful instructions here!")
@@ -35,14 +88,27 @@ def help():
         main()
 
 def options_menu():
-    print("Here will be additional options like importing, highscores etc")
-    start = str(input("Type 'start' and press enter to continue"))
-    if start=='start':
+    print("To view the scores for the Gothic quiz type 'scoresG'. To view the scores for the Witcher quiz type 'scoresW'. To play type 'start'.")
+    option = str(input())
+    if option=='start':
         main()
+    elif option=='scoresG':
+        scores = open('resultsG.csv')
+        scoreReader = csv.reader(scores)
+        scoreData = list(scoreReader)
+        print(scoreData)
+        scores.close()
+    elif option=='scoresW':
+        scores = open('resultsW.csv')
+        scoreReader = csv.reader(scores)
+        scoreData = list(scoreReader)
+        print(scoreData)
+        scores.close()
 
 def main():
     while True:
-        choice= str(input("Welcome to Quizmania!. Press 1 to begin a Gothic quiz or press 2 to begin a Witcher quiz, then press enter. Type 'help' and press enter for more instructions. For more options: type 'options' and press enter."))
+        choice= str(input("Welcome to Quizmania!. Press 1 to begin a Gothic quiz or press 2 to begin a Witcher quiz, then press enter."
+                          " Type 'help' and press enter for more instructions. For more options: type 'options' and press enter."))
         if choice!="1" and choice!="2" and choice!='options' and choice!='help':
             print("Something went wrong. Try again.")
             continue
@@ -57,4 +123,5 @@ def main():
                 help()
             break
 
-main()
+if __name__ == '__main__':
+    main()
