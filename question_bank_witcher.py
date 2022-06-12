026789391f946b5916362e9235ef570f053f0e75
Albert Neczajewski <100307127+albertnikim@users.noreply.github.com>
2022-06-12 13:36:23 +0200

    Add files via upload

---
+++ question_bank_witcher.py
@@ -0,0 +1,12 @@
+question_bank_witcher = {
+    'Which country is led by Radovid? A.Redania B.Temeria C.Cintra D.None of the above': 'A',
+    'What is the name of the main character? A.Thomas B.Frodo C.Witcher D.Geralt': 'D',
+    'Who is responsible for the murder of Vesemir? A.Bandits B.Nilfgardian soldiers C.The Wild Hunt D.Geralt': 'C',
+    'Which of these is a friend of the main character? A.Jaskier B.Zoltan C.Eskel D.Regis': 'ABCD',
+    'Which of these weapons are available in the game? A.Crossbows B.Guns C.Swords D.Lances': 'AC',
+    'Was Foltest a king of Temeria? Type True or False': 'True',
+    'Is Yennefer a swordmaster? Type True or False': 'False',
+    'The adopted daughter of Geralt is called C_____a Enter the missing letters': 'irill',
+    'The final boss in the game is called E____n Enter the missing letters': 'redi',
+    'The author of the Witcher books is Andrzej S_______i Enter the missing letters': 'apkowsk',
+}
